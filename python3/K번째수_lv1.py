2c9a38aede04e53f91163c0871aeadbedc55c421
eoqkrskfk94 <37679062+eoqkrskfk94@users.noreply.github.com>
2019-12-30 11:40:02 +0900

    K번째수 문제풀이
    K번째수 문제풀이

---
+++ python3/K번째수_lv1.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i in commands:
+        temp = []
+        temp = sorted(array[i[0]-1:i[1]])
+        answer.append(temp[i[2]-1])
+    return answer
